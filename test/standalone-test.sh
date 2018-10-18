9cdd5e5e2ff74e3b5c602493fdbc730d0a84d3cb
Hajime Tazaki <thehajime@gmail.com>
2018-10-19 00:06:18 +0900

    add hello world, python standalone tests

--- test/standalone-test.sh
+++ test/standalone-test.sh
@@ -4,23 +4,43 @@ mkdir -p /tmp/bundle/rootfs
 mkdir -p /tmp/runu-root
 
 # get script from moby
-curl -O https://raw.githubusercontent.com/moby/moby/master/contrib/download-frozen-image-v2.sh
-bash download-frozen-image-v2.sh . thehajime/runu-base:$TRAVIS_OS_NAME
+curl https://raw.githubusercontent.com/moby/moby/master/contrib/download-frozen-image-v2.sh \
+     -o /tmp/download-frozen-image-v2.sh
+bash /tmp/download-frozen-image-v2.sh /tmp/ thehajime/runu-base:$TRAVIS_OS_NAME
 
 # extract images from layers
-for layer in `find ./ -name layer.tar`
+for layer in `find /tmp/ -name layer.tar`
 do
  tar xvfz $layer -C /tmp/bundle/rootfs
 done
 
 rm -f config.json
 ./runu spec
+
+
+run_test()
+{
+    ./runu --debug --root=/tmp/runu-root run --bundle=/tmp/bundle foo
+    sleep 5
+    ./runu --debug --root=/tmp/runu-root kill foo 9
+    ./runu --debug --root=/tmp/runu-root delete foo
+}
+
+# test hello-world
+cat config.json | jq '.process.args |=["hello"] ' > /tmp/1
+mv /tmp/1 config.json
+cp config.json /tmp/bundle/
+run_test
+
+# test ping
 cat config.json | jq '.process.args |=["ping","--","127.0.0.1"] ' > /tmp/1
 mv /tmp/1 config.json
 cp config.json /tmp/bundle/
+run_test
 
-./runu --debug --root=/tmp/runu-root run --bundle=/tmp/bundle foo
-sleep 5
-./runu --debug --root=/tmp/runu-root kill foo 9
-./runu --debug --root=/tmp/runu-root delete foo
+# test python
+cat config.json | jq '.process.args |=["python", "imgs/python.iso", "imgs/python.img", "--", "-c", "print(\"hello world\")"] ' > /tmp/1
+mv /tmp/1 config.json
+cp config.json /tmp/bundle/
+run_test
 

28885768e54e028505404542d325537d71f1ef6e
Hajime Tazaki <thehajime@gmail.com>
2020-03-09 18:35:07 +0900

    fix kind image build, limiting to amd64/linux image
    Signed-off-by: Hajime Tazaki <thehajime@gmail.com>

--- pkg/post-deploy-kind-img.sh
+++ pkg/post-deploy-kind-img.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+# XXX: need multi-arch image build
+if [ $TRAVIS_ARCH != "amd64" ] || [ $TRAVIS_OS_NAME != "linux" ] ; then
+    echo "This now only builds linux/amd64 image. Skipping"
+    exit 0
+fi
+
 cp $TRAVIS_HOME/gopath/bin/${RUNU_PATH}runu k8s/
 cd k8s
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin

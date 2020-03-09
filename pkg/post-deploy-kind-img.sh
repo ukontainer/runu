2e24d415a543c0381a725f54e5d47b9a56c6a7e9
Hajime Tazaki <thehajime@gmail.com>
2020-03-09 17:37:35 +0900

    fix kind deployment script
    Signed-off-by: Hajime Tazaki <thehajime@gmail.com>

--- pkg/post-deploy-kind-img.sh
+++ pkg/post-deploy-kind-img.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-cp runu k8s
+cp $TRAVIS_HOME/gopath/bin/${RUNU_PATH}runu k8s/
 cd k8s
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker build -t thehajime/node-runu:v1.17.0 .

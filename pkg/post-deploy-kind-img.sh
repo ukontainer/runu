9b9103e3aa63164aaf156ba54bf5b831b84bdf0b
Hajime Tazaki <thehajime@gmail.com>
2020-03-19 22:10:51 +0900

    add k8s test using KinD
    Signed-off-by: Hajime Tazaki <thehajime@gmail.com>

--- pkg/post-deploy-kind-img.sh
+++ pkg/post-deploy-kind-img.sh
@@ -6,9 +6,4 @@ if [ $TRAVIS_ARCH != "amd64" ] || [ $TRAVIS_OS_NAME != "linux" ] ; then
     exit 0
 fi
 
-cp $TRAVIS_HOME/gopath/bin/${RUNU_PATH}runu k8s/
-cd k8s
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker build -t thehajime/node-runu:v1.17.0 .
 docker push thehajime/node-runu:v1.17.0
-cd ..
---
+++ test/k8s-test.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+. $(dirname "${BASH_SOURCE[0]}")/common.sh
+
+KIND_VERSION=v0.7.0
+
+# XXX: need multi-arch image build
+if [ $TRAVIS_ARCH != "amd64" ] || [ $TRAVIS_OS_NAME != "linux" ] ; then
+    echo "This now only builds linux/amd64 image. Skipping"
+    exit 0
+fi
+
+# Kind preparation
+fold_start k8s.test.0 "k8s: KinD image preparation"
+
+# Build kind node docker image
+cp $TRAVIS_HOME/gopath/bin/${RUNU_PATH}runu k8s/
+cd k8s
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+docker build -t thehajime/node-runu:v1.17.0 .
+cd ..
+
+fold_end k8s.test.0 ""
+
+fold_start k8s.test.1 "k8s: install kind cli"
+
+GO111MODULE="on" go get sigs.k8s.io/kind@$KIND_VERSION
+
+# install kubectl
+curl -LO \
+     https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
+chmod +x kubectl
+sudo mv kubectl /usr/local/bin/
+
+fold_end k8s.test.1 ""
+
+
+fold_start k8s.test.2 "k8s: kind setup"
+kind create cluster --image thehajime/node-runu:v1.17.0 --config k8s/kind-cluster.yaml
+kubectl get pods -o wide -A
+kubectl get nodes -o wide -A
+
+sleep 60
+
+# install runtime class
+kubectl apply -f k8s/ukontainer-runtimeclass.yaml
+
+fold_end k8s.test.2 ""
+
+fold_start k8s.test.3 "k8s: hello world"
+# install runu pod
+cat k8s/hello-world.yaml | sed "s/\$DOCKER_IMG_VERSION/$DOCKER_IMG_VERSION/" \
+    | kubectl apply -f -
+
+kubectl get nodes -o wide -A
+sleep 20
+kubectl get pods -o wide -A
+kubectl logs deployment/helloworld-runu |& tee /tmp/log.txt
+grep "icmp_req=1" /tmp/log.txt
+
+fold_end k8s.test.3 ""

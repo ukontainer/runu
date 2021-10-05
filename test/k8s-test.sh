de830551c5c0ad700088591e7d77b3469f02165f
Hajime Tazaki <thehajime@gmail.com>
2021-10-05 18:41:21 +0900

    ci: add calico test for k8s
    Signed-off-by: Hajime Tazaki <thehajime@gmail.com>

--- test/k8s-test.sh
+++ test/k8s-test.sh
@@ -2,6 +2,8 @@
 
 . $(dirname "${BASH_SOURCE[0]}")/common.sh
 
+DST_ADDR=$1
+
 # XXX: need multi-arch image build
 if [ $TRAVIS_ARCH != "amd64" ] || [ $TRAVIS_OS_NAME != "linux" ] ; then
     echo "This now only builds linux/amd64 image. Skipping"
@@ -19,12 +21,11 @@ fold_start k8s.test.3 "k8s: hello world"
 # install runu pod
 ## XXX: github action runners don't allow to pass ICMP at firewall
 cat k8s/hello-world.yaml | sed "s/\$DOCKER_IMG_VERSION/$DOCKER_IMG_VERSION/" \
-    | sed "s/8.8.8.8/127.0.0.1/" \
-    | sed "s%ukontainer\/runu-base%${REGISTRY}ukontainer\/runu-base%" \
+    | sed "s/8.8.8.8/$DST_ADDR/" \
     | kubectl apply -f -
 
 kubectl get nodes -o wide -A
-sleep 20
+sleep 60
 set -x
 kubectl get pods -o wide -A
 kubectl describe deployment/helloworld-runu

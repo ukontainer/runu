28d615d9f287da98c638808eaa2a4f3f6af48b0a
Hajime Tazaki <thehajime@gmail.com>
2024-07-22 23:18:04 +0900

    fix amd64 debian pkg build/test
    Signed-off-by: Hajime Tazaki <thehajime@gmail.com>

--- pkg/pre-deploy-test-deb.sh
+++ pkg/pre-deploy-test-deb.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 sudo apt-get install ./$PACKAGE_FILENAME
-DOCKER_RUN_ARGS="run --rm -i -e RUMP_VERBOSE=1 -e DEBUG=1 --runtime=runu-dev --net=none $DOCKER_RUN_ARGS_ARCH"
+sudo systemctl restart docker
+sudo cat /etc/docker/daemon.json
+DOCKER_RUN_ARGS="run --rm -i -e RUMP_VERBOSE=1 -e DEBUG=1 --runtime=runu --net=none $DOCKER_RUN_ARGS_ARCH"
 
 docker $DOCKER_RUN_ARGS $DOCKER_RUN_EXT_ARGS alpine uname -a
